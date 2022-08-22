78caabf17499657b267fc719dd77e504a15c09a4
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-08-22 16:09:36 +0200

    Create areas.jl

---
+++ data/b-areas/areas.jl
@@ -0,0 +1,77 @@
+## straight segments
+
+### square
+
+A = a^2
+a = A^0.5
+d = a * 2^0.5
+
+### rectangle
+
+A = a * b
+d = (A^2 + b^2)^0.5
+
+### parallelogram
+
+A = a * h = a * b * sin(alpha)
+d_1 = ((a + h * cot(alpha))^2 + h^2)^0.5
+d_2 = ((a - h * cot(alpha))^2 + h^2)^0.5
+
+### trapezium
+
+A = (a + b)/2 * h = m * h
+m = (a + b)/2
+
+### triangle
+
+A = (a * h)/2 = r * s = (s * (s - a) * (s - b) * (s - c))^0.5
+r = (a * h)/(2 * s)
+R = (b * c)/(2 * h)
+s = (a + b + c)/2
+
+### equilateral triangle
+
+A = (a^2/4) * 3^0.5
+h = (a/2) * 3^0.5
+
+### pentagon
+
+A = (5/8) * r^2 * (10 + 2 * 5^0.5)^0.5
+a = (1/2) * r * (10 - 2 * 5^0.5)^0.5
+rho = (1/4) * r * (6 + 2 * 5^0.5)^0.5
+#### construction
+AB = 0.5 * r, BC = BD, CD = CE
+
+### hexagon
+
+(TODO)
+
+### octagon
+
+(TODO)
+
+### polygon
+
+A = A_1 + A_2 + A_3 = 0.5 * (a*h_1 + b*h_2 + c*h_3)
+
+## curved segments
+
+### circle
+
+(TODO)
+
+### annulus
+
+(TODO)
+
+### sector of a circle
+
+(TODO)
+
+### segment of a circle
+
+(TODO)
+
+### ellipse
+
+(TODO)
